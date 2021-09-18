399cbc9bf60a1075203d0384d7f7622ba5f939d1
Bryan Patrick <bryan_lol_@hotmail.com>
2021-09-18 20:09:38 -0300

    feito home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <h1 class="logo">Vila dos sonhos</h1>
+      <ul >
+       | <li></li> |
+      </ul>
+    </header>
+    <div>
+      <p>Bem vindo ao nosso Web Site!!</p>
+      <img src="../assets/img-hotel.jpg" alt="">
+    </div>
+    <section>
+      <div>
+        <img src="../assets/img-faixada.jpg" alt="">
+      </div>
+      <section>
+        <div>
+          <img src="../assets/img-hall.jpg" alt="">
+          <img src="../assets/img-restaurante.jpg" alt="">
+          <img src="../assets/img-corredores.jpg" alt="">
+        </div>
+        <div>
+          <img src="../assets/img-vista.jpg" alt="">
+          <img src="../assets/img-spa.jpg" alt="">
+          <img src="../assets/img-piscina.jpg" alt="">
+        </div>
+      </section>
+    </section>
+    <footer>
+        REDES SOCIAIS
+    </footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    
+  },
+  data() {
+    return {
+      navBar : 
+      [{label: "Home"}, {label: "Acomodações"}, {label: "Endereço"},
+      {label: "Contato"}
+      ]
+    }
+  },
+  props: ["Navigation"]
 }
 </script>
